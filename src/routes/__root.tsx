540792a1d538a5807a71025604785aeefc773e1b
shekokamal7775-png <shekokamal7775@gmail.com>
2026-07-29 05:39:15 +0300

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -117,12 +117,20 @@ function RootShell({ children }: { children: React.ReactNode }) {
         <HeadContent />
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(){var s=document.createElement('script');s.src='https://quge5.com/88/tag.min.js';s.async=true;s.setAttribute('data-zone','263284');s.setAttribute('data-cfasync','false');document.head.appendChild(s);})();`
-          }}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(s){s.dataset.zone='11427556',s.src='https://al5sm.com/tag.min.js'})([document.documentElement,document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
+            __html: `
+(function(){
+  var s1=document.createElement('script');
+  s1.src='https://quge5.com/88/tag.min.js';
+  s1.async=true;
+  s1.setAttribute('data-zone','263284');
+  s1.setAttribute('data-cfasync','false');
+  document.head.appendChild(s1);
+})();
+(function(s){
+  s.dataset.zone='11427556';
+  s.src='https://al5sm.com/tag.min.js';
+})([document.documentElement,document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
+            `
           }}
         />
       </head>
