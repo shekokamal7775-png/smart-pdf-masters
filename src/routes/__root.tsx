f763cea8be0180d9e6e01d66e7c03ae21af32a8a
shekokamal7775-png <shekokamal7775@gmail.com>
2026-07-29 14:21:38 +0300

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -115,24 +115,7 @@ function RootShell({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-(function(){
-  var s1=document.createElement('script');
-  s1.src='https://quge5.com/88/tag.min.js';
-  s1.async=true;
-  s1.setAttribute('data-zone','263284');
-  s1.setAttribute('data-cfasync','false');
-  document.head.appendChild(s1);
-})();
-(function(s){
-  s.dataset.zone='11427556';
-  s.src='https://al5sm.com/tag.min.js';
-})([document.documentElement,document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
-            `
-          }}
-        />
+        <script src="/monetag.js" async={true} />
       </head>
       <body>
         {children}
